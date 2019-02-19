66e6fe76fc5cbf36d4e9737fd0471f29b903b921
CalWal <callumtrwallace@gmail.com>
2019-02-19 17:27:00 +1000

    Updated structure

--- folio/src/App.js
+++ folio/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Header from "/components/Header";
-import Photo from "/components/Photo";
+import Header from "./components/Header";
+import Photo from "./components/Photo";
 import ContactInfo from "./components/ContactInfo";
 
 class App extends Component {
