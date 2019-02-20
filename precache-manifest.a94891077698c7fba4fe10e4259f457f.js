5b63cc9e43c3207c69aa5d35ddf5fcca9a97b267
CalWal <callumtrwallace@gmail.com>
2019-02-20 17:40:04 +1000

    Updates

---
+++ precache-manifest.a94891077698c7fba4fe10e4259f457f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Folio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a8711aec57106c2ae42",
+    "url": "/Folio/static/js/runtime~main.2a8711ae.js"
+  },
+  {
+    "revision": "7b155bc4860fa0eb1102",
+    "url": "/Folio/static/js/main.7b155bc4.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/Folio/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "7b155bc4860fa0eb1102",
+    "url": "/Folio/static/css/main.f8b50bfd.chunk.css"
+  },
+  {
+    "revision": "267dab00ee73e700b3be66196559228e",
+    "url": "/Folio/index.html"
+  }
+];
